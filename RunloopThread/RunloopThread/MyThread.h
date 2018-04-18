18b1cba288fc12a1dded073918060a3aa89d6153
SionChen <759856708@qq.com>
2018-04-18 13:35:09 +0800

    - -

---
+++ RunloopThread/RunloopThread/MyThread.h
@@ -0,0 +1,13 @@
+//
+//  MyThread.h
+//  RunloopThread
+//
+//  Created by 超级腕电商 on 2018/4/18.
+//  Copyright © 2018年 超级腕电商. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyThread : NSThread
+
+@end
---
+++ RunloopThread/RunloopThread/MyThread.m
@@ -0,0 +1,13 @@
+//
+//  MyThread.m
+//  RunloopThread
+//
+//  Created by 超级腕电商 on 2018/4/18.
+//  Copyright © 2018年 超级腕电商. All rights reserved.
+//
+
+#import "MyThread.h"
+
+@implementation MyThread
+
+@end
